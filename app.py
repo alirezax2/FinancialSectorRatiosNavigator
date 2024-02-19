833139bd184d1c93b8210b70ab8bc0064744da10
alirezax2 <alireza.naziri@gmail.com>
2024-02-19 23:14:40 +0100

    add ticker widget

--- app.py
+++ app.py
@@ -25,13 +25,16 @@ todaydate = pn.widgets.DatePicker(
 todaydate2 = datetime.today().strftime("%Y-%m-%d")   
 df = get_df(todaydate2)
 selectedcol = pn.widgets.Select(name='Select Ratio', value = 'PEG Ratio', options=list(df.columns))
-selecteditem = pn.widgets.Select(name='Select Item', options=['Industry' , 'Sector'])
+selecteditem = pn.widgets.Select(name='Select Item', value='Sector' , options=['Industry' , 'Sector'])
 selectedmethod = pn.widgets.Select(name='Select Method', value= 'Mean' , options=['Mean', 'Min' , 'Max'])
+
+ticker = pn.widgets.AutocompleteInput(name='Ticker', options=list(df.Ticker) , placeholder='Write Ticker here همین جا')
+ticker.value = "AAPL"
 
 selectedhover = 'Ticker'
 hv.extension('bokeh')
 
-def create_plot(selectedcol,selecteditem,todaydate,selectedmethod):
+def create_plot(selectedcol,selecteditem,todaydate,selectedmethod,ticker):
     df = get_df(todaydate)
     if selectedmethod=='Mean':
         group_them = df.groupby(selecteditem)[selectedcol].mean()
@@ -40,16 +43,16 @@ def create_plot(selectedcol,selecteditem,todaydate,selectedmethod):
     if selectedmethod=='Max':
         group_them = df.groupby(selecteditem)[selectedcol].max()
     df2 = df.merge(group_them, left_on=selecteditem, right_index=True, suffixes=('', f'_{selecteditem}_{selectedmethod}'))
-
-    return df2.hvplot.bar(x=selecteditem, y=f'{selectedcol}_{selecteditem}_{selectedmethod}', hover_cols=selectedhover, height=800, width=1800).opts(xrotation=90, fontsize={'xticks': 10}).opts(show_grid=True)
+    df3 = df.query('Ticker == @ticker')
+    return df2.hvplot.bar(x=selecteditem, y=f'{selectedcol}_{selecteditem}_{selectedmethod}', hover_cols=selectedhover, height=800, width=1800).opts(xrotation=90, fontsize={'xticks': 10}).opts(show_grid=True) * df3.hvplot.bar(x=selecteditem, y=f'{selectedcol}',hover_cols=selectedhover,)
 
 def create_alert(selectedcol):
     text = f"### {selectedcol} \n {DescriptionDict[selectedcol]}"
     return pn.pane.Alert(text, alert_type="warning")
     
 
-bound_plot = pn.bind(create_plot, selectedcol=selectedcol , selecteditem=selecteditem, todaydate=todaydate, selectedmethod=selectedmethod)
+bound_plot = pn.bind(create_plot, selectedcol=selectedcol , selecteditem=selecteditem, todaydate=todaydate, selectedmethod=selectedmethod,ticker=ticker)
 bound_alert = pn.bind(create_alert,selectedcol=selectedcol)
-pn.Column(pn.Row(selectedcol, selecteditem, todaydate,selectedmethod), bound_plot, bound_alert).servable(title="Financial Sector Ratios Navigator")
+pn.Column(pn.Row(selectedcol, selecteditem, todaydate,selectedmethod,ticker), bound_plot, bound_alert).servable(title="Financial Sector Ratios Navigator")
 
 
